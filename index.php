3c77d728f7190beef54f3231bfcdbefa272cdddd
Steve Montgomery <steve@chums.com>
2025-03-05 13:42:23 -0700

    chore: php updates

--- index.php
+++ index.php
@@ -1,31 +1,15 @@
 <?php
 
 
-/**
- * @package Chums
- * @subpackage ProjectedDemands
- * @author Steve Montgomery
- * @copyright Copyright &copy; 2013, steve
- */
+use chums\ui\WebUI2;
+require_once 'autoload.inc.php';
 
-require_once ("autoload.inc.php");
-require_once ('access.inc.php');
-
-$bodyPath = "/apps/time-clock";
-$title = "Chums Time Clock";
-$description = "";
-
-$ui = new WebUI($bodyPath, $title, $description, false, 5);
-$ui->version = "2020.10.09";
-$ui->bodyClassName = 'container';
-$ui->AddCSS("public/main.css", true);
-$ui->addManifest('public/js/manifest.json');
-//$ui->AddJS("public/js/manifest.d41d8cd98f00b204e980.js");
-//$ui->addChunkManifest('public/js/chunk-manifest.json');
-/**
- * Changelog:
- */
-
-$ui->RequireLogin(false);
-
-$ui->Send();
+$ui = new WebUI2([
+    'title' => 'Chums Time Clock',
+    'bodyClassName' => 'container',
+    'contentFile' => 'body.inc.php',
+    'requiredRoles' => [],
+    'requireLogin'  => false,
+]);
+$ui->addManifestJSON('public/js/manifest.json')
+    ->render();
